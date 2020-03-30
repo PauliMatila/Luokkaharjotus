82b8c7f403dc2f22ae2231b17a66246dad3e5913
Pauli Matila <pauli.matila@outlook.com>
2020-03-30 14:01:14 +0300

    Add project files.

---
+++ Luokkaharjotus/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Luokkaharjotus
+{
+    public class Person
+    {
+        public string name;
+        public int age;
+        public int salary;
+        public Person friend;
+
+        public Person(string name, int age, int salary)
+        {
+            this.name = name;
+            this.age = age;
+            this.salary = salary;
+        }
+
+        //public Person()
+        //{
+
+        //}
+
+        //public Person (Person friend)
+        //{
+        //    this.friend = friend;
+        //}
+
+        //public Person (string name, int age)
+        //{
+        //    this.name = name;
+        //    this.age = age;
+        //}
+    }
+    
+}
---
+++ Luokkaharjotus/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Luokkaharjotus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> tyypit = new List<Person>();
+            tyypit.Add(new Person("Matti", 30, 900));
+            tyypit.Add(new Person("Timo", 26, 1300));
+            tyypit.Add(new Person("Toni", 34, 2100));
+            
+            foreach (var dude in tyypit)
+            {
+                Console.WriteLine($"nimi: {dude.name} ikä: {dude.age} palkka: {dude.salary}");
+            }
+
+            int yhteinenikä = 0;
+            foreach (Person dude in tyypit)
+            {
+                yhteinenikä += dude.age;
+            }
+            Console.WriteLine(yhteinenikä);
+            
+            /*Person person1 = new Person();
+            Person person2 = new Person();
+            Person person3 = new Person();
+            Person person4 = new Person("Mikko", 38);
+            Person person5 = new Person("Tommi", 41);
+            Person ystävä1 = new Person();
+            Person ystävä2 = new Person();
+            Person ystävä3 = new Person();
+
+
+            person1.name = "Pauli";
+            person1.name = "Matti";
+            person1.age = 28;
+            person1.age = 30;
+            person1.salary = 900;
+            person1.salary = 1100;
+
+            person2.name = "Kalle";
+            person2.name = "Timo";
+            person2.age = 20;
+            person2.age = 26;
+            person2.salary = 1500;
+            person2.salary = 1300;
+
+            person3.name = "Toni";
+            person3.age = 34;
+            person3.salary = 2100;
+
+            Console.WriteLine(person1.name);
+            Console.WriteLine(person1.age);
+            Console.WriteLine(person1.salary);
+
+            Console.WriteLine(person2.name);
+            Console.WriteLine(person2.age);
+            Console.WriteLine(person2.salary);
+
+            Console.WriteLine(person3.name);
+            Console.WriteLine(person3.age);
+            Console.WriteLine(person3.salary);
+
+            Console.WriteLine($"{person4.name} {person4.age}");
+            Console.WriteLine($"{person5.name} {person5.age}");
+            Console.WriteLine($"{Pekka.friend} {Antti.friend}");
+            */
+        }
+    }
+}
